091558d279646ab8871ff447ab21346a47b5a0ec
davidjungermann <david.jungermann@gmail.com>
2023-09-08 13:59:04 +0200

    Updates script and brew

--- script.sh
+++ script.sh
@@ -1,1 +1,8 @@
+# Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/repos/personal/config/.zshrc
+
 ln -s ~/repos/personal/config/.zshrc ~/.zshrc
