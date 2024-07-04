bbe8f888d39bcf6f083af21ebb5bfe929e7395d6
David Jungermann <djungermann@Davids-MBP.localdomain>
2024-07-04 14:25:36 +0200

    Updates Brewfile

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the location of your custom .zshrc
-TARGET_ZSHRC=~/repos/personal/config/.zshrc
+TARGET_ZSHRC=~/repos/config/.zshrc
 
 # Create a symbolic link. If ~/.zshrc exists, this will overwrite it!
 ln -sf $TARGET_ZSHRC ~/.zshrc
