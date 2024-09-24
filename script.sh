1e3f9bef2384fd3801d88dd9967589d8d2e1c2c5
David Jungermann <david@swimbird.se>
2024-09-24 11:18:51 +0200

    Adds global gitignore

--- script.sh
+++ script.sh
@@ -6,4 +6,6 @@ TARGET_ZSHRC=~/repos/config/.zshrc
 # Create a symbolic link. If ~/.zshrc exists, this will overwrite it!
 ln -sf $TARGET_ZSHRC ~/.zshrc
 
+git config --global core.excludesfile ~/repos/config/.gitignore_global
+
 echo "$TARGET_ZSHRC has been updated and symlinked to ~/.zshrc!"
